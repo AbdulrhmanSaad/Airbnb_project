38998fd167ca0124553f75de854c78a914487cac
AbdulrhmanSaad <106705332+AbdulrhmanSaad@users.noreply.github.com>
2022-06-03 15:51:51 +0200

    Add files via upload

--- Airbnb_system/Place_Manager.h
+++ Airbnb_system/Place_Manager.h
@@ -1,35 +1,23 @@
 #include "Place.h"
 class Place_Manager
 {
-
 public:
 	Valid v;
-	// map< string, vector <Place*> > place_list;
-	////typedef std::map< string, vector <Place*> > place_list;
-	////static place_list place_list_;
 	static std::map< string, vector <Place*> > place_list_;
-	/*Place_Manager(const Place_Manager&) = delete;
-	void operator=(const Place_Manager&) = delete;*/
 public:
-
 	Place_Manager();
 	void display_filter();
 	void add_filter(vector <Place*>&);
-	/*
-	-------------search----------------
-	*/
 	int filter_number();
 	void select_search();
 	void edit_place(string&);
 	void add_place();//only implemented in cpp to show how class works
 	void func(vector<Place*>&);
-	//void search_duration(vector<Place*>&);
 	void search_duration();
 	void search_price();
 	void search_date();
 	void access();
-	void remove_connect_places(string);
-	void remove_filter();
+	void remove_filter(vector <Place*>);
 	void display_places();//display All system places
 	void No_filter_display(vector <Place*>& city_places);//display city places
 	vector<pair<int, char>> display_selected_filter();//display filter vector
@@ -39,9 +27,6 @@ public:
 	void view_start_date(vector<Place*>&);
 	void view_end_date(vector<Place*>&);
 	void view_duration(vector<Place*>&);
-	void admin_delete();
-	//datastructure search(city)-->map
-	//datastructure search(price) -->tree
 	~Place_Manager();
 };
 
